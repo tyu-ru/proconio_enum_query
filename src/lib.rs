698d62bcb3c49285c20acb31935cc0ad1393e3dd
tyu-ru <tyuru.tw.cpp@gmail.com>
2021-02-16 02:18:23 +0900

    refactor: Extract method

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
-use proc_macro::TokenStream;
-use proc_macro2::Literal;
+// use proc_macro::;
+use proc_macro2::{Literal, TokenStream};
 use quote::quote;
-use syn::{parse_macro_input, Fields, Item, Type};
+use syn::{parse_macro_input, Fields, FieldsNamed, FieldsUnnamed, Ident, Item, Type};
 
 #[proc_macro_attribute]
-pub fn derive_query(_args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn derive_query(
+    _args: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
     let item = if let Item::Enum(x) = parse_macro_input!(input as Item) {
         x
     } else {
@@ -16,57 +19,19 @@ pub fn derive_query(_args: TokenStream, input: TokenStream) -> TokenStream {
         .into();
     };
 
-    // eprintln!("{:#?}", item);
-
     let mut out = item.clone();
     for v in &mut out.variants {
-        match &mut v.fields {
-            Fields::Unnamed(unnamed) => {
-                for field in &mut unnamed.unnamed {
-                    let ty = &field.ty;
-                    let ident = quote! { <#ty as proconio::source::Readable>::Output };
-                    field.ty = Type::Verbatim(ident.into());
-                }
-            }
-            Fields::Named(named) => {
-                for field in &mut named.named {
-                    let ty = &field.ty;
-                    let ident = quote! { <#ty as proconio::source::Readable>::Output };
-                    field.ty = Type::Verbatim(ident.into());
-                }
-            }
-            Fields::Unit => {}
-        };
+        type_convert_to_readable_output(&mut v.fields);
     }
 
-    let enum_name = &item.ident;
+    let enum_ident = &item.ident;
 
     let matcher = item.variants.iter().enumerate().map(|(i, v)| {
-        let ident = &v.ident;
+        let variant_ident = &v.ident;
         let stmt = match &v.fields {
-            Fields::Unnamed(unnamed) => {
-                let fields = &unnamed.unnamed;
-                let len = fields.len();
-                let input_fields = if len == 1 {
-                    quote! { (#fields,) }
-                } else {
-                    quote! { (#fields) }
-                };
-                let i = (0..len).map(|i| Literal::usize_unsuffixed(i));
-                let output_fields= quote! { #(temp.#i),* };
-                quote! { proconio::input!{ from source, temp: #input_fields } #enum_name::#ident(#output_fields) }
-            }
-            Fields::Named(named) => {
-                let fields = &named.named;
-                let (temp_input, temp_output):(Vec<_>,Vec<_>) = fields.iter().map(|f|{
-                    let (ident, ty) = (f.ident.as_ref().expect("parse error?"), &f.ty);
-                    (quote!{ #ident:#ty }, quote!{ #ident })
-                }).unzip();
-                quote! { proconio::input! { from source, #(#temp_input),*} #enum_name::#ident{ #(#temp_output),* } }
-            }
-            Fields::Unit => {
-                quote! { #enum_name::#ident }
-            }
+            Fields::Unnamed(unnamed) => tuple_field_input(enum_ident, variant_ident, unnamed),
+            Fields::Named(named) => strct_field_input(enum_ident, variant_ident, named),
+            Fields::Unit => quote! { #enum_ident::#variant_ident },
         };
         let i = Literal::usize_unsuffixed(i);
         quote! { #i => { #stmt }}
@@ -75,7 +40,7 @@ pub fn derive_query(_args: TokenStream, input: TokenStream) -> TokenStream {
     let gen = quote! {
         #out
 
-        impl proconio::source::Readable for #enum_name {
+        impl proconio::source::Readable for #enum_ident {
             type Output = Self;
             fn read<R: std::io::BufRead, S: proconio::source::Source<R>>(source: &mut S) -> Self::Output {
                 let t = source.next_token_unwrap().parse::<usize>().expect("query number parse error");
@@ -87,4 +52,57 @@ pub fn derive_query(_args: TokenStream, input: TokenStream) -> TokenStream {
         }
     };
     gen.into()
+}
+
+fn type_convert_to_readable_output(fields: &mut Fields) {
+    match fields {
+        Fields::Unnamed(FieldsUnnamed {
+            unnamed: ref mut fields,
+            ..
+        })
+        | Fields::Named(FieldsNamed {
+            named: ref mut fields,
+            ..
+        }) => {
+            for field in fields {
+                let ty = &field.ty;
+                let ident = quote! { <#ty as proconio::source::Readable>::Output };
+                field.ty = Type::Verbatim(ident.into());
+            }
+        }
+        Fields::Unit => {}
+    }
+}
+
+fn tuple_field_input(
+    enum_ident: &Ident,
+    variant_ident: &Ident,
+    field: &FieldsUnnamed,
+) -> TokenStream {
+    let fields = &field.unnamed;
+    let len = fields.len();
+    let input_fields = if len == 1 {
+        quote! { (#fields,) }
+    } else {
+        quote! { (#fields) }
+    };
+    let i = (0..len).map(|i| Literal::usize_unsuffixed(i));
+    let output_fields = quote! { #(temp.#i),* };
+    quote! { proconio::input!{ from source, temp: #input_fields } #enum_ident::#variant_ident(#output_fields) }
+}
+
+fn strct_field_input(
+    enum_ident: &Ident,
+    variant_ident: &Ident,
+    field: &FieldsNamed,
+) -> TokenStream {
+    let fields = &field.named;
+    let (temp_input, temp_output): (Vec<_>, Vec<_>) = fields
+        .iter()
+        .map(|f| {
+            let (ident, ty) = (f.ident.as_ref().expect("parse error?"), &f.ty);
+            (quote! { #ident:#ty }, quote! { #ident })
+        })
+        .unzip();
+    quote! { proconio::input! { from source, #(#temp_input),*} #enum_ident::#variant_ident{ #(#temp_output),* } }
 }
