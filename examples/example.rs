20b0aadaf04ce83af20ba8c968ab6bc60752f627
tyu-ru <tyuru.tw.cpp@gmail.com>
2021-02-16 05:53:26 +0900

    doc: Add example

---
+++ examples/example.rs
@@ -0,0 +1,26 @@
+use proconio::{input, marker::*, source::auto::AutoSource};
+
+#[proconio_enum_query::derive_query]
+#[derive(PartialEq, Debug)]
+enum Query {
+    A(Usize1),
+    B,
+    C { x: i64, y: char },
+}
+
+fn main() {
+    let source = AutoSource::from(
+        r#"1 12
+           2
+           3 34 X"#,
+    );
+
+    input! {
+        from source,
+        query: [Query; 3],
+    }
+
+    assert_eq!(query[0], Query::A(12 - 1));
+    assert_eq!(query[1], Query::B);
+    assert_eq!(query[2], Query::C { x: 34, y: 'X' });
+}
